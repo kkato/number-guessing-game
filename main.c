946fa3762488a933c1227a21ae5bb3a5ef32254a
kkato <kkato149@gmail.com>
2024-09-22 18:17:19 +0900

    initianl commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int target, guess;
+
+    // 乱数の種を設定
+    srand(time(0));
+    target = rand() % 100 + 1;  // 1から100までのランダムな数字
+
+    printf("1から100までの数を当ててください。\n");
+
+    // 正解するまで繰り返す
+    do {
+        printf("数を入力してください: ");
+        scanf("%d", &guess);
+
+        if (guess > target) {
+            printf("もっと小さいです。\n");
+        } else if (guess < target) {
+            printf("もっと大きいです。\n");
+        } else {
+            printf("正解です！\n");
+        }
+    } while (guess != target);
+
+    return 0;
+}
